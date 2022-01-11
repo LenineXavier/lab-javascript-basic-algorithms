67117ad3490e9163da939dc1eda622b86656f719
Lenine Xavier <leninecxavier@gmail.com>
2022-01-11 08:48:31 -0300

    done

--- js/index.js
+++ js/index.js
@@ -18,9 +18,26 @@ console.log(`The navigator's name is ${hacker2} `)
 
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
 console.log(hacker1.toUpperCase().split("").join(" "));
+
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
 let reverseHacker2 = hacker2.split('').reverse().join('');
 console.log(reverseHacker2);
+//3.3 Depending on the lexicographic order of the strings
+//Ordem lexigráfica letras maíusculas precedem mínusculas 
 
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+} ;
 
 
